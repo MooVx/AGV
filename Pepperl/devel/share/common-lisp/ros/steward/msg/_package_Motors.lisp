0be5fe975e35d824eefb63b7f3f8112ade7165c3
pawel <pwegrzyn54@gmail.com>
2017-08-30 12:13:57 +0200

    ignore

--- Pepperl/devel/share/common-lisp/ros/steward/msg/_package_Motors.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package steward-msg)
-(cl:export '(MOTOR1_PWM-VAL
-          MOTOR1_PWM
-          MOTOR2_PWM-VAL
-          MOTOR2_PWM
-))
--- Pepperl/devel/share/steward/cmake/steward-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(steward_MSG_INCLUDE_DIRS "/home/pawel/AGV/Pepperl/src/steward/msg;/home/pawel/AGV/Pepperl/devel/share/steward/msg")
-set(steward_MSG_DEPENDENCIES std_msgs;actionlib_msgs)
