30bcb228abbd381d591bc0b5f35b9fa054a207dd
Ilona <ili.ili@interia.pl>
2017-10-23 16:37:47 +0200

    changed path planner

--- Pepperl/src/steward/src/path_planner.py
+++ Pepperl/src/steward/src/path_planner.py
@@ -3,9 +3,11 @@
 import rospy
 from geometry_msgs.msg import Twist
 from steward.msg import Camera
+from steward.msg import STMdata
 from numpy import abs
 
 timeout = 0.3
+state = 'stop'
 
 cam_data = Camera()
 last_cam_cb = 0
@@ -25,16 +27,31 @@ def create_cmd_vel_from_cam(camera_data):
     return vel_msg
 
 
+def stm_callback(data):
+    global state
+       
+    if state == 'stop' and data.start_button:
+        state = 'run'
+    if data.safety_button:
+        state = 'stop'
+
+
 
 rospy.init_node("path_planner")
-
+rospy.Subscriber('STM_data', STMdata, stm_callback)
 rospy.Subscriber("Camera_data", Camera, cam_cb)
+
 cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
 rate = rospy.Rate(10)
 
 while not rospy.is_shutdown():
-    if rospy.get_time() - last_cam_cb < timeout:
-        cmd_vel_pub.publish(create_cmd_vel_from_cam(cam_data))
+
+    if state == 'run':
+        if rospy.get_time() - last_cam_cb < timeout:
+            cmd_vel_pub.publish(create_cmd_vel_from_cam(cam_data))
+        else:
+            cmd_vel_pub.publish(Twist())
     else:
         cmd_vel_pub.publish(Twist())
+
     rate.sleep()
