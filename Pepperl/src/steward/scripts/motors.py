1778a578b84b6bca4c44abc57f80c9b01fe6a736
Ilona <ili.ili@interia.pl>
2017-10-23 13:54:21 +0200

    added blocking cmd_vel if button not pressed

--- Pepperl/src/steward/scripts/motors.py
+++ Pepperl/src/steward/scripts/motors.py
@@ -10,6 +10,9 @@ from steward.msg import STMdata
 lm = Polulu_driver(15, 'left_motor_driver')
 rm = Polulu_driver(15, 'right_motor_driver')
 
+timeout = 0.3
+last_safety_ok = 0.0
+
 def callback(data):
     global lm, rm, left_sp_pub, right_sp_pub
     # lm.wake_up()
@@ -30,21 +33,29 @@ def callback(data):
     # rm.set_speed(scaled_r)
 
 def left_callback(data):
-    global lm
+    global lm, last_safety_ok, timeout
     lm.wake_up()
-    lm.set_speed(int(data.data))
+    if rospy.get_time() - last_safety_ok < timeout:
+        lm.set_speed(int(data.data))
+    else:
+        lm.set_speed(0)
 
 def right_callback(data):
-    global rm
+    global rm, last_safety_ok
     rm.wake_up()
-    rm.set_speed(int(data.data))
+    if rospy.get_time() - last_safety_ok < timeout:
+        rm.set_speed(int(data.data))
+    else:
+        rm.set_speed(0)
 
 def stm_callback(data):
-    global left_state_pub, right_state_pub, last_cb
+    global left_state_pub, right_state_pub, last_cb, last_safety_ok
 
     dt = rospy.get_time() - last_cb
     last_cb = rospy.get_time()
 
+    if data.safety_button:
+        last_safety_ok = rospy.get_time()
 
     #not mistake!
     speed_left = data.n_cntR
@@ -59,6 +70,7 @@ def stm_callback(data):
     speed_left = float(speed_left) * 2 * 3.14  / ( 2048.0 * dt)
     speed_right = float(speed_right) * 2 * 3.14  / ( 2048.0 * dt)
     
+
     left_state_pub.publish(Float64(speed_left))
     right_state_pub.publish(Float64(speed_right))
 
