fd56193613b97556b00fbb1b92edbd903acdec75
Ilona <ili.ili@interia.pl>
2017-10-24 16:04:15 +0200

    debug

--- Pepperl/src/steward/src/path_planner.py
+++ Pepperl/src/steward/src/path_planner.py
@@ -28,13 +28,13 @@ def create_cmd_vel_from_cam(camera_data):
     k_x = 0.2
     vel_msg = Twist()
     if camera_data.lanes > 0:
-        if (camera_data.angle > 0 and camera_data.pos_y < -10.0 ) or (camera_data.angle < 0 and camera_data.pos_y > 10.0):
+        if (camera_data.angle > 0 and camera_data.pos_y < -15.0 ) or (camera_data.angle < 0 and camera_data.pos_y > 15.0):
             vel_msg.linear.x = 0.0
         else:
             vel_msg.linear.x = 0.2
 
         if (abs(camera_data.angle) < 30 and abs(camera_data.pos_y) > 10.0) or vel_msg.linear.x == 0.0:
-            vel_msg.angular.z = -k_th * camera_data.pos_y/300.0
+            vel_msg.angular.z = -k_th * camera_data.pos_y/200.0
         else:
             vel_msg.angular.z  = -camera_data.angle/ 100.0
         
